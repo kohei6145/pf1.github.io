93e399e81ed13275ed66a0c75a1999b2c793af95
kohei6145 <68057193+kohei6145@users.noreply.github.com>
2021-02-16 19:44:25 +0900

    Add files via upload

---
+++ prac1/index.js
@@ -0,0 +1,9 @@
+"use strict";
+{
+    const open = document.getElementById("open");
+    const overlay = document.querySelector(".overlay");
+    
+    open.addEventListener("click", () => {
+        overlay.classList.toggle("show");
+    });
+}
